91157ebad66db78f1e0a6359c703e327d0fb8b85
amal_jith <123450863+amal-jith@users.noreply.github.com>
2024-09-28 14:56:19 +0530

    Update forms.py

--- recipe_suggester/recipes/forms.py
+++ recipe_suggester/recipes/forms.py
@@ -1,7 +1,4 @@
 from django import forms
 
 class IngredientForm(forms.Form):
-    ingredients = forms.CharField(
-        widget=forms.Textarea(attrs={'rows': 3}),
-        label="Enter a list of ingredients"
-    )
+    ingredients = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}), label="Enter a list of ingredients")
